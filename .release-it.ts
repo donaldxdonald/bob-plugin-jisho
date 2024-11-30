923a2e4a60710a02b25a8050b1d964d5f73397ef
donaldxdonald <mzhlovefama@gmail.com>
2024-11-30 16:28:17 +0800

    chore: unset release note

--- .release-it.ts
+++ .release-it.ts
@@ -8,10 +8,6 @@ const config: Config = {
     release: true,
     assets: ['bob-plugin-jisho-*.bobplugin'],
     autoGenerate: true,
-    releaseNotes(context: any) {
-      // Remove the first, redundant line with version and date.
-      return context.changelog.split('\n').slice(1).join('\n')
-    },
   },
   hooks: {
     'before:github:release': 'pnpm run bundle',
